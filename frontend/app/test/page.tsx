0fa34ac09dfa0fc0eef748d339e9783b07bb84f2
hafizd <hafizdvalorant@gmail.com>
2025-06-13 09:39:45 +0700

    hafizd: add register page and test file

---
+++ frontend/app/test/page.tsx
@@ -0,0 +1,11 @@
+import Dashboard from "../components/main/SideBar";
+import BlobCursor from "../components/ui/BlobCursor";
+
+export default function DashboardPage() {
+  return (
+    <>
+      <Dashboard/>
+      <h1 className="text-4xl">Test Screen</h1>
+    </>
+  );
+}
