39ea1b4b9bf94f9741177890859d426e7915c219
hafizd <hafizdvalorant@gmail.com>
2025-06-13 09:36:10 +0700

    hafid: update landing page

--- frontend/app/components/HomeSection.tsx
+++ frontend/app/components/HomeSection.tsx
@@ -1,12 +1,32 @@
-// app/components/HomeSection.tsx
+"use client";
+
 import Image from "next/image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCamera, faGraduationCap } from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
+import { useState, useEffect } from "react";
 
 export default function HomeSection() {
+  const [target, setTarget] = useState("/login");
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    const hasToken = document.cookie.includes("token=");
+    if (hasToken) {
+      setTarget("/dashboard");
+    }
+
+    // Trigger animasi saat komponen mount
+    setTimeout(() => setShow(true), 100); // delay kecil agar transisi smooth
+  }, []);
+
   return (
-    <section id="home" className="py-16 px-4 h-90vh">
+    <section
+      id="home"
+      className={`py-16 px-4 h-90vh transition-all duration-1000 ease-out transform ${
+        show ? "opacity-100 translate-y-0" : "opacity-0 translate-x-8"
+      }`}
+    >
       <div className="container mx-auto flex flex-col md:flex-row items-center">
         <div className="md:w-1/2 mb-10 md:mb-0">
           <h1 className="text-5xl md:text-6xl font-bold mb-6 title-font text-blue-800">
@@ -18,11 +38,11 @@ export default function HomeSection() {
           </p>
           <div className="flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-4">
             <Link
-              href="#learn"
+              href={target}
               className="bg-yellow-500 flex gap-x-2 items-center hover:bg-yellow-600 text-blue-900 font-bold py-3 px-6 rounded-full text-center transition-all transform hover:scale-105 shadow-lg"
             >
               {/* <FontAwesomeIcon icon={`fa-solid fa-camera`}/> */}
-            <FontAwesomeIcon icon={faGraduationCap} className="w-8" />
+              <FontAwesomeIcon icon={faGraduationCap} className="w-8" />
               Start Learning
             </Link>
             <Link
