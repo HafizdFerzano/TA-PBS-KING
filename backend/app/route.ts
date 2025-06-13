86e861f03ad1e15b1811a9f45f75e861387d1479
rhivo <rhivoaprianto123@gmail.com>
2025-06-13 16:06:26 +0700

    rhivo: update route

--- backend/app/route.ts
+++ backend/app/route.ts
@@ -1,5 +1,16 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  return NextResponse.json({ message: "API KING" });
+  return NextResponse.json({
+    metadata: {
+      status: 200,
+      error: 0,
+      message: "Welcome to Shape Explorer API",
+    },
+    data: {
+      version: "1.0.0",
+      documentation: "/api/docs",
+      availableRoutes: ["/api/user", "/api/shape", "/api/qrarcode"],
+    },
+  });
 }
